9b87168db281383a542a710c6c2da1231e10a5ac
franco_t <franco_t@etna-alternance.net>
2018-11-16 14:25:15 +0100

    add table stats

--- src/pages/graphs/graphs.ts
+++ src/pages/graphs/graphs.ts
@@ -17,6 +17,37 @@ import { GlobalVars } from '../../services/settings.service'
 export class GraphsPage {
 
 	public colorClassName: string;
+	public selectedFile: string;
+	public listFileParse: Array<object> = [
+		{
+			"name": "test1",
+			"values": "test1"
+		},
+		{
+			"name": "test2",
+			"values": "test2"
+		},
+	];
+
+	public selectedColumn: Array<string>;
+	public listColumns: Array<object> = [
+		{
+			"name": "col1",
+			"values": "col1"
+		},
+		{
+			"name": "col2",
+			"values": "col2"
+		},
+		{
+			"name": "col3",
+			"values": "col3"
+		},
+		{
+			"name": "col4",
+			"values": "col4"
+		},
+	];
 
 	constructor(public navCtrl: NavController, public navParams: NavParams, public globalVars: GlobalVars) {
 	}
